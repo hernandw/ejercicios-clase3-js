455b0f38e1cf6e3d0c41f0b7440bb943ffa5266a
Williams Hernández <hernandw@gmail.com>
2024-02-07 10:34:52 -0300

    :tada: ejercicios con funciones y ciclos

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+//Funcion:  una series de instrucciones agrupadas que podemos reutilizar
+
+
+//Mostrando los diferente formas del console.log(
+
+
+let playerOne = 120
+let playerTwo = 130
+let playerThree = 140
+let playerFour = 150
+let playerFive = 160
+
+console.log('console.log', playerOne)
+console.debug('console.debug', playerTwo)
+console.warn('console.warn', playerThree)
+console.info('console.info', playerFour)
+console.error('console.error', playerFive)
