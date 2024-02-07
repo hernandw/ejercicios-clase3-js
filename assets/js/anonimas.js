6767b5b49fe87614874714800e332f669ee43065
Williams Hernández <hernandw@gmail.com>
2024-02-07 11:21:36 -0300

    sparkles: funciones anonimas

---
+++ assets/js/anonimas.js
@@ -0,0 +1,40 @@
+
+
+let sumarDosNumeros = function(num1, num2) {
+    let suma = num1 + num2
+
+    console.log(suma)
+}
+
+let num1Usuario = +prompt('Ingresa un numero')
+let num2Usuario = +prompt('Ingresa otro numero')
+
+
+sumarDosNumeros(num1Usuario, num2Usuario)
+
+/* let saludo = function(name) {
+    console.log('Hola buenos dias ¿como estas?', name)
+}
+
+saludo('Juan')
+saludo('Williams')
+saludo('Andrea')
+saludo('Albamar')
+
+
+function() {
+    //bloque de instrucciones
+} */
+/* 
+let saluda = function () {
+    console.log('Hola como estas')
+}
+
+saluda()
+
+let greeting = function () {
+  console.log("Hola como estas");
+};
+
+
+greeting() */
