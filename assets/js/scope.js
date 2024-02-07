17a296dd5e2bc9916900af93200cc2727a1b58bc
Williams Hernández <hernandw@gmail.com>
2024-02-07 11:44:45 -0300

    :sparkles: scope de una variable

---
+++ assets/js/scope.js
@@ -0,0 +1,32 @@
+'use strict'
+
+// Scope de las variables: es el ambito donde se desenvuelven las variable, puede ser global, funcion o de bloque
+
+
+var numero = 15
+var age = 25
+
+if (age) {
+    
+    let name = 'Juan'
+    
+}
+
+
+console.log(numero)
+console.log(age)
+console.log(name)
+
+//var por inicializarlas varias veces
+
+var numero2 = 25
+console.log(numero2)
+var numero2 = 80
+console.log(numero2);
+
+
+let numero3 = 3
+console.log(numero3)
+
+
+
